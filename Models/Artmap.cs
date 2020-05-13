520b3afe7d4c540c3aa6a2039664108e3addd104
GForrow <garyforrowbsc@gmail.com>
2020-05-13 16:16:08 +0100

    added ArtMap table and migrated

---
+++ Models/Artmap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreSqlDb.Models
+{
+    public class Artmap
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+    }
+}
+
